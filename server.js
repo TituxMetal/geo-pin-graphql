7d9f3a1cc02e58e9fab7722a868f7e0302c3cb2e
Desktop | Tuxi Metal <github@lgdweb.fr>
2019-11-12 07:08:51 +0100

    Update server.js file; add mongoose connection

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const { ApolloServer } = require('apollo-server')
+const mongoose = require('mongoose')
 
 const typeDefs = require('./typeDefs')
 const resolvers = require('./resolvers')
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => process.stdout.write('Db connected!'))
+  .catch((error) => process.stdout.write(error))
 
 const server = new ApolloServer({
   typeDefs,
