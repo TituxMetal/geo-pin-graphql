77a9e9408b2c8e1f0d1d8628b5680b44ae8a0336
Desktop | Tuxi Metal <github@lgdweb.fr>
2019-11-12 07:32:57 +0100

    Update Auth Login component; add Google login basic functionality

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import { GoogleLogin } from 'react-google-login'
+import { GraphQLClient } from 'graphql-request'
 import { withStyles } from '@material-ui/core/styles'
-// import Typography from "@material-ui/core/Typography";
+// import Typography from "@material-ui/core/Typography"
 
-const Login = ({ classes }) => <div>Login</div>
+const ME_QUERY = `
+  query {
+    me {
+      _id
+      name
+      email
+      picture
+    }
+  }
+`
+const clientId = process.env.REACT_APP_OAUTH_CLIENT_ID
+const graphqlUri = process.env.REACT_APP_GRAPHQL_URI
+
+const Login = ({ classes }) => {
+  const onSuccess = async (googleUser) => {
+    const idToken = googleUser.getAuthResponse().id_token
+    const client = new GraphQLClient(graphqlUri, {
+      headers: {
+        authorization: idToken
+      }
+    })
+    const data = await client.request(ME_QUERY)
+    console.log({ data })
+  }
+
+  return <GoogleLogin clientId={clientId} onSuccess={onSuccess} isSignedIn />
+}
 
 const styles = {
   root: {
