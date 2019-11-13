12f225b4960798d42f9c9d6b7cd59893a5158472
Desktop | Tuxi Metal <github@lgdweb.fr>
2019-11-13 03:25:13 +0100

    Update Header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,36 +1,64 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
-// import AppBar from "@material-ui/core/AppBar";
-// import Toolbar from "@material-ui/core/Toolbar";
-// import Map from "@material-ui/icons/Map";
-// import Typography from "@material-ui/core/Typography";
+import React, { useContext } from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import MapIcon from '@material-ui/icons/Map'
+import Typography from '@material-ui/core/Typography'
+
+import Context from '../context'
+import Signout from './Auth/Signout'
 
 const Header = ({ classes }) => {
-  return <div>Header</div>;
-};
+  const { state } = useContext(Context)
+  const { currentUser } = state
 
-const styles = theme => ({
+  return (
+    <header className={classes.root}>
+      <AppBar position='static'>
+        <Toolbar>
+          <section className={classes.grow}>
+            <MapIcon className={classes.icon} />
+            <Typography component='h1' variant='h6' color='inherit' noWrap>
+              GeoPin
+            </Typography>
+          </section>
+          {currentUser && (
+            <section className={classes.grow}>
+              <img className={classes.picture} src={currentUser.picture} alt={currentUser.name} />
+              <Typography variant='h5' color='inherit' noWrap>
+                {currentUser.name}
+              </Typography>
+            </section>
+          )}
+          <Signout />
+        </Toolbar>
+      </AppBar>
+    </header>
+  )
+}
+
+const styles = (theme) => ({
   root: {
     flexGrow: 1
   },
   grow: {
     flexGrow: 1,
-    display: "flex",
-    alignItems: "center"
+    display: 'flex',
+    alignItems: 'center'
   },
   icon: {
     marginRight: theme.spacing.unit,
-    color: "green",
+    color: 'green',
     fontSize: 45
   },
   mobile: {
-    display: "none"
+    display: 'none'
   },
   picture: {
-    height: "50px",
-    borderRadius: "90%",
+    height: '50px',
+    borderRadius: '90%',
     marginRight: theme.spacing.unit * 2
   }
-});
+})
 
-export default withStyles(styles)(Header);
+export default withStyles(styles)(Header)
