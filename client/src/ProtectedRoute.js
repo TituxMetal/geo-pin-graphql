5506b9196b60fdc03ea60c0c6e71489b4f2ddbed
Desktop | Tuxi Metal <github@lgdweb.fr>
2019-11-13 02:07:47 +0100

    Add ProtectedRoute component; protect the home page if the user is not authenticated

---
+++ client/src/ProtectedRoute.js
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react'
+import { Redirect, Route } from 'react-router-dom'
+
+import Context from './context'
+
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  const { state } = useContext(Context)
+  const render = (props) => (!state.isAuth ? <Redirect to='/login' /> : <Component props={props} />)
+
+  return (
+    <Route
+      render={render}
+      props={rest}
+    />
+  )
+}
+
+export default ProtectedRoute
