6bdaec991698eeb46f027d715b32474672370fa6
Desktop | Tuxi Metal <github@lgdweb.fr>
2019-11-14 03:19:18 +0100

    Update reducer.js file; add create draft and update draft action types

--- client/src/reducer.js
+++ client/src/reducer.js
@@ -19,6 +19,21 @@ const reducer = (state, { type, payload }) => {
         isAuth: false
       }
     }
+    case 'CREATE_DRAFT': {
+      return {
+        ...state,
+        draft: {
+          longitude: 0,
+          latitude: 0
+        }
+      }
+    }
+    case 'UPDATE_DRAFT_LOCATION': {
+      return {
+        ...state,
+        draft: payload
+      }
+    }
     default: {
       return state
     }
