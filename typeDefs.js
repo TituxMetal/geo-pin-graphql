935476083f07b3368ba4c9d413cd413d2bf36b44
Desktop | Tuxi Metal <github@lgdweb.fr>
2019-11-22 10:48:08 +0100

    Update typeDefs.js file; update _id to id, add CreatePinInput input, add getPins query, add createPin and deletePin mutations

--- typeDefs.js
+++ typeDefs.js
@@ -2,14 +2,14 @@ const { gql } = require('apollo-server')
 
 module.exports = gql`
   type User {
-    _id: ID!
+    id: ID!
     name: String!
     email: String!
     picture: String
   }
 
   type Pin {
-    _id: ID!
+    id: ID!
     title: String!
     content: String!
     image: String!
@@ -26,7 +26,21 @@ module.exports = gql`
     createdAt: String!
   }
 
+  input CreatePinInput {
+    title: String!
+    image: String!
+    content: String!
+    latitude: Float!
+    longitude: Float!
+  }
+
   type Query {
     me: User!
+    getPins: [Pin!]
+  }
+
+  type Mutation {
+    createPin(input: CreatePinInput!): Pin!
+    deletePin(pinId: ID!): Pin!
   }
 `
