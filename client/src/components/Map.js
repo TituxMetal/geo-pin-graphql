80f91d4322053c6ba1d8dafe4b81753252622527
Desktop | Tuxi Metal <github@lgdweb.fr>
2019-11-22 11:09:46 +0100

    Update Map component; get all pins, add a popup when click on a pin and delete a user pin when authenticated

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,12 +1,17 @@
-import React, { useState, useEffect, useContext } from 'react'
-import ReactMapGL, { NavigationControl, Marker } from 'react-map-gl'
+import React, { useState, useEffect, useContext, useCallback } from 'react'
+import ReactMapGL, { NavigationControl, Marker, Popup } from 'react-map-gl'
+import differenceInMinutes from 'date-fns/difference_in_minutes'
 import { withStyles } from '@material-ui/core/styles'
-// import Button from '@material-ui/core/Button'
-// import Typography from '@material-ui/core/Typography'
-// import DeleteIcon from '@material-ui/icons/DeleteTwoTone'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import DeleteIcon from '@material-ui/icons/DeleteTwoTone'
 
+import Blog from './Blog'
 import PinIcon from './PinIcon'
 import Context from '../context'
+import useGqlClient from '../hooks/useGqlClient'
+import { getPinsQuery } from '../graphql/queries'
+import { deletePinMutation } from '../graphql/mutations'
 
 const initialViewport = {
   latitude: 37.75,
@@ -18,23 +23,46 @@ const Map = ({ classes }) => {
   const { state, dispatch } = useContext(Context)
   const [viewport, setViewport] = useState(initialViewport)
   const [userPosition, setUserPosition] = useState(null)
+  const [popup, setPopup] = useState(null)
+  const client = useGqlClient()
 
-  useEffect(() => {
-    getUserPosition()
+  const getAllPins = useCallback(async () => {
+    const { getPins } = await client.request(getPinsQuery)
+    dispatch({ type: 'GET_ALL_PINS', payload: getPins })
   }, [])
 
-  const getUserPosition = () => {
+  useEffect(() => {
+    getAllPins()
+  }, [getAllPins])
+
+  const getUserPosition = useCallback(() => {
     if ('geolocation' in navigator) {
       navigator.geolocation.getCurrentPosition(position => {
         const { latitude, longitude } = position.coords
-        setViewport({ ...viewport, latitude, longitude })
+        setViewport(vport => ({ ...vport, latitude, longitude }))
         setUserPosition({ latitude, longitude })
       })
     }
+  }, [])
+
+  useEffect(() => {
+    getUserPosition()
+  }, [getUserPosition])
+
+  const handleDeletePin = async pin => {
+    const variables = { pinId: pin.id }
+    const { deletePin } = await client.request(deletePinMutation, variables)
+
+    dispatch({ type: 'DELETE_PIN', payload: deletePin })
+    setPopup(null)
   }
 
-  const handleMapClick = ({ lngLat, leftButton }) => {
-    if (!leftButton) return
+  const handleMapClick = ({ target, lngLat, leftButton }) => {
+    if (!leftButton || target.childNodes.length < 1) return
+
+    if (popup) {
+      setPopup(null)
+    }
 
     if (!state.draft) {
       dispatch({ type: 'CREATE_DRAFT' })
@@ -44,6 +72,24 @@ const Map = ({ classes }) => {
 
     dispatch({ type: 'UPDATE_DRAFT_LOCATION', payload: { longitude, latitude } })
   }
+
+  const handleClosePopup = () => {
+    setPopup(null)
+    dispatch({ type: 'CLEAR_CURRENT_PIN' })
+  }
+
+  const highlightNewPin = pin => {
+    const isNewPin = differenceInMinutes(Date.now(), Number(pin.createdAt)) <= 30
+
+    return isNewPin ? 'limegreen' : 'darkblue'
+  }
+
+  const handleSelectPin = pin => {
+    setPopup(pin)
+    dispatch({ type: 'SET_PIN', payload: pin })
+  }
+
+  const isAuthUser = () => state.currentUser.id === popup.author.id
 
   return (
     <div className={classes.root}>
@@ -79,7 +125,37 @@ const Map = ({ classes }) => {
             <PinIcon size={40} color='hotpink' />
           </Marker>
         )}
+        {state.pins.map(pin => (
+          <Marker
+            key={pin.id}
+            latitude={pin.latitude}
+            longitude={pin.longitude}
+            offsetLeft={-19}
+            offsetTop={-37}
+          >
+            <PinIcon size={40} color={highlightNewPin(pin)} onClick={() => handleSelectPin(pin)} />
+          </Marker>
+        ))}
+        {popup && (
+          <Popup anchor='top' latitude={popup.latitude} longitude={popup.longitude} closeOnClick={false} onClose={handleClosePopup}>
+            <img className={classes.popupImage} src={popup.image} alt={popup.title} />
+            <div className={classes.popupTab}>
+              <Typography>
+                {popup.latitude.toFixed(6)}
+                ,
+                {' '}
+                {popup.longitude.toFixed(6)}
+              </Typography>
+              {isAuthUser() && (
+                <Button onClick={() => handleDeletePin(popup)}>
+                  <DeleteIcon className={classes.deleteIcon} />
+                </Button>
+              )}
+            </div>
+          </Popup>
+        )}
       </ReactMapGL>
+      <Blog />
     </div>
   )
 }
