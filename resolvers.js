25bbd8e5e5719542c43f4342d6a55701add3c405
Desktop | Tuxi Metal <github@lgdweb.fr>
2019-11-22 10:51:23 +0100

    Update resolvers.js file; pass arguments to next in authenticated middleware function, update me query with arguments, add getPins query function, add createPin and deletePin mutation functions

--- resolvers.js
+++ resolvers.js
@@ -1,15 +1,38 @@
 const { AuthenticationError } = require('apollo-server')
+const Pin = require('./models/Pin')
 
 const authenticated = next => (_root, _args, context, _info) => {
   if (!context.currentUser) {
     throw new AuthenticationError('You must be logged in!')
   }
 
-  return next(context)
+  return next(_root, _args, context, _info)
 }
 
 module.exports = {
   Query: {
-    me: authenticated(context => context.currentUser)
+    me: authenticated((_root, _args, context, _info) => context.currentUser),
+    getPins: async (_root, _args, _context, _info) => {
+      const pins = await Pin.find().populate('author').populate('comments.author')
+
+      return pins
+    }
+  },
+  Mutation: {
+    createPin: authenticated(async (_root, { input }, { currentUser }, _info) => {
+      const newPin = await new Pin({
+        ...input,
+        author: currentUser.id
+      }).save()
+
+      const pinAdded = await Pin.populate(newPin, 'author')
+
+      return pinAdded
+    }),
+    deletePin: authenticated(async (_root, { pinId }, { currentUser }, _info) => {
+      const pinDeleted = await Pin.findOneAndDelete({ _id: pinId }).exec()
+
+      return pinDeleted
+    })
   }
 }
